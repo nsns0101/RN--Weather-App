46a5711b754ee47db33586efc6e4885dc8cd4cba
nsns0101 <nsns0101@naver.com>
2020-01-20 21:31:37 +0900

    create_Screen_Loading

--- App.js
+++ App.js
@@ -1,40 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-
-      <View style={styles.yellowView}>
-        <Text style={styles.text}>Hello</Text>
-      </View>
-
-      <View style={styles.blueView}>
-        <Text>Hello</Text>
-      </View>
-    </View>
-  );
+  return <Loading />;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // flexDirection: "row",
-    // backgroundColor: 'red',
-    // alignItems: 'center',
-    // justifyContent: 'center',
-  },
-  text: {
-    color: "black",
-    fontSize: 15,
-  },
-  yellowView: {
-    flex: 2,          //
-    backgroundColor: "yellow"
-  },
-
-  blueView: {
-    flex: 3,
-    backgroundColor: "blue"
-  },
-});
