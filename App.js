71782f2e98e2983608fca8e00a22d7c7b86098b1
nsns0101 <nsns0101@naver.com>
2020-01-21 20:18:03 +0900

    get - Location, Weather

--- App.js
+++ App.js
@@ -1,7 +1,49 @@
 import React from 'react';
-import Loading from "./Loading";
+import Loading from "./Loading";            //현재파일내의 Loading.js
+import * as Location from "expo-location"   //위치정보
+import { Alert } from "react-native";       //경고창
+import axios from "axios";                  //
 
-export default function App() {
-  return <Loading />;
+//https://home.openweathermap.org/api_keys
+const API_KEY = "4f63d82ac712979f28797a28187a3d28"
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+
+  //날씨정보 얻어오는 함수
+  getWeather = async (latitude, longitude) => {
+    //&units=metric는 섭씨온도
+    const { data } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`);
+    console.log(data);
+  };
+
+  //위치정보 얻오는 함수
+  getLocation = async () => {
+    try {
+      const response = await Location.requestPermissionsAsync();   //권한 요청
+      // console.log(response)
+      // { }로 객체로 선언
+      const { coords } = await Location.getCurrentPositionAsync();  //현재위치를 가져옴
+      //latitude = 위도, longitude = 경도
+      this.getWeather(coords.latitude, coords.longitude)
+      this.setState({ isLoading: false });
+
+    }
+    //권한 요청을 거부할 경우 
+    catch (error) {
+      console.log(error)
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    console.log(isLoading);
+    return isLoading ? <Loading /> : null;
+  }
 }
 
--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 ## > 날씨앱 <
-- 참고 사이트 : https://academy.nomadcoders.co/courses/enrolled/216885
+
+# 
+- 참고 강의 사이트 : https://academy.nomadcoders.co/courses/enrolled/216885
+# 
+- 참고 위치정보 메서드 사이트 : https://docs.expo.io/versions/v36.0.0/sdk/location/locationgetcurrentpositionasyncoptionsgeolocation#__docusaurus
+
+# 날씨 API 사이트 
+- API_KEY : https://home.openweathermap.org/api_keys
+
 
 # NPM, Android Studio 설치
 - NPM : https://nodejs.org/ko/download/
@@ -14,3 +22,9 @@
 # 프로젝트 실행
 - 1. AndroidStudio 에뮬레이터 실행
 - 2. expo start로 프로젝트 실행(package.json 참고)
+
+# 위치정보 받아오기
+- npm install expo-location
+- 사용할 때는 import * as Location from "expo-location"
+
+
