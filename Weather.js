e86a967f588adeb4923f1c3ece738438873d42fb
nsns0101 <nsns0101@naver.com>
2020-01-29 19:45:16 +0900

    create_text___title,subtitle

--- Weather.js
+++ Weather.js
@@ -7,48 +7,68 @@ import { LinearGradient } from 'expo-linear-gradient';      // linear-gradient
 
 //날씨옵션
 const weatherOptions = {
-    Clouds: {       //흐림, 안개
+    Clouds: {       //흐림
         iconName: "weather-cloudy",
-        gradient: ["#4DA0B0", "#D39D38"]
+        gradient: ["#D7D2CC", "#304352"],
+        title: "흐림",
+        subtitle: "비가 올 수 있으니 우산을 챙기세요!",
 
     },
     Clear: {        //맑음
         iconName: "weather-sunny",
-        gradient: ["#b2fefa", "#0ed2f7"]
+        gradient: ["#FEF253", "#FF7300"],
+        title: "맑음",
+        subtitle: "화창한 날씨입니다!",
 
     },
     Thunderstorm: { //번개
         iconName: "weather-lightning",
-        gradient: ["#304352", "#d7d2cc"]
+        gradient: ["#373B44", "#4286f4"],
+        title: "번개",
+        subtitle: "번개가 치며, 비가 올 수 있으니 우산을 챙기세요!",
     },
     Drizzle: {   //가랑비
         iconName: "weather-hail",
-        gradient: ["#2980b9", "#2c3e50"]
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "약한 비",
+        subtitle: "약한 비가 내리고 있습니다.",
     },
     Rain: {     //비
         iconName: "weather-???",
-        gradient: ["#4b79a1", "#283e51"]
+        gradient: ["#00C6FB", "#005BEA"],
+        title: "비",
+        subtitle: "비가 오니 외출 시 우산을 챙기세요",
     },
     Snow: {     //눈
         iconName: "weather-snowy",
-        gradient: ["#c0c0aa", "#1cefff"]
+        gradient: ["#7DE2FC", "#B9B6E5"],
+        title: "눈",
+        subtitle: "눈이 오고 있으니 외출시 조심하세요",
     },
-    // Atmosphere: {
-    //     iconName: "weather-???",
-    //     gradient: [,]
-    // },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"],
+        title: "안개",
+        subtitle: "안개로 앞이 잘 보이지 않을 수 있습니다",
+    },
     Haze: {     //안개
         iconName: "weather-fog",
-        gradient: ["#44a08d", "#093637"]
+        gradient: ["#44a08d", "#093637"],
+        title: "짙은 안개",
+        subtitle: "안개로 앞이 잘 보이지 않을 수 있습니다",
     },
     Mist: {  //옅은 안개
         iconName: "weather-weather-fog",
-        gradient: ["#44a08d", "#093637"]
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "옅은 안개",
+        subtitle: "안개로 앞이 잘 보이지 않을 수 있습니다",
     },
-    // Dust: {     //먼지
-    //     iconName: "weather-???",
-    //     gradient: [,]
-    // },
+    Dust: {     //먼지
+        iconName: "weather-fog",
+        gradient: ["#4DA0B0", "#D39D38"],
+        title: "먼지",
+        subtitle: "외부활동시 마스크가 필요할 수 있습니다",
+    },
 }
 
 export default function Weather({ temp, condition }) {
@@ -73,9 +93,14 @@ export default function Weather({ temp, condition }) {
             </View >
 
             {/* 글 */}
-            <View style={styles.halfContainer} />
+            <View style={styles.halfContainer} >
+                <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+                    <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                    <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
+                </View>
+            </View>
 
-        </LinearGradient>
+        </LinearGradient >
 
 
     );
@@ -112,5 +137,21 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+    },
+    title: {
+        color: "white",
+        fontSize: 44,
+        fontWeight: "300",
+        marginBottom: 10,
+    },
+    subtitle: {
+        color: "white",
+        fontSize: 24,
+        fontWeight: "600",
+
+    },
+    textContainer: {
+        paddingHorizontal: 20,
+        alignItems: "flex-start"
     }
 });
